1675eaaae901ae3deb0dea0dc49db44e53e73597
Joe Fleming <w33ble@users.noreply.github.com>
2019-02-28 18:33:49 -0700

    chore: remove esm (#3)
    Bumps esm, makes it a dev dependency, and adds a build pipeline to publish commonjs version of the module instead. esm is no longer a run-time dependency.

--- test/parse_test_output.mjs
+++ test/parse_test_output.mjs
@@ -1,3 +1,4 @@
+/* eslint no-console: 0 */
 import fs from 'fs';
 import path from 'path';
 import assert from 'assert';
@@ -17,28 +18,30 @@ const specs = {
 };
 
 Object.keys(specs)
-  .reduce((acc, browser) => {
-    return acc.then(() => {
-      const spec = specs[browser];
-      const caps = { browserName: browser };
+  .reduce(
+    (acc, browser) =>
+      acc.then(() => {
+        const spec = specs[browser];
+        const caps = { browserName: browser };
 
-      return gravyTap(src, { capabilities: caps })
-        .then(results => {
-          // throw is rejection was expected
-          if (spec.rejects) throw new Error('Runner was expected to reject');
+        return gravyTap(src, { capabilities: caps })
+          .then(results => {
+            // throw is rejection was expected
+            if (spec.rejects) throw new Error('Runner was expected to reject');
 
-          assert.strictEqual(results.tests, spec.total, `${spec.total} total tests`);
-          assert.strictEqual(results.passed, spec.pass, `${spec.pass} tests pass`);
-          assert.strictEqual(results.failed, spec.fail, `${spec.fail} tests fail`);
-          console.log(`${browser} - assertions passed`);
-        })
-        .catch(err => {
-          // swallow rejection is it's expected
-          if (!spec.rejects) throw err;
-          console.log(`${browser} - assertions passed`);
-        });
-    });
-  }, Promise.resolve())
+            assert.strictEqual(results.tests, spec.total, `${spec.total} total tests`);
+            assert.strictEqual(results.passed, spec.pass, `${spec.pass} tests pass`);
+            assert.strictEqual(results.failed, spec.fail, `${spec.fail} tests fail`);
+            console.log(`${browser} - assertions passed`);
+          })
+          .catch(err => {
+            // swallow rejection is it's expected
+            if (!spec.rejects) throw err;
+            console.log(`${browser} - assertions passed`);
+          });
+      }),
+    Promise.resolve()
+  )
   .catch(err => {
     console.log('Output assertions failed');
     console.error(err);
