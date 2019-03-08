d16d6ce4258b3927c8b48a352fd1920fcf21c85c
joe fleming <joe.fleming@gmail.com>
2019-03-07 19:17:27 -0700

    chore: confirm test bundle is es5

--- .rollup.test.js
+++ .rollup.test.js
@@ -1,8 +1,9 @@
 import resolve from 'rollup-plugin-node-resolve';
 import buble from 'rollup-plugin-buble';
+import assertEs from 'rollup-plugin-assert-es';
 
 export default {
   input: 'test/tape.mjs',
   output: [{ format: 'umd', file: 'test/bundle.js' }],
-  plugins: [resolve({ browser: true }), buble()],
+  plugins: [resolve({ browser: true }), buble(), assertEs()],
 };
